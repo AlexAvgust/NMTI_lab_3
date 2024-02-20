ab5c43fe96924ba5f53e2840793a551eade06c2a
Oleksii Trufanov <trufanov_alex@outlook.com>
2024-02-20 17:45:43 +0200

    split main into smaller classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,71 +1,27 @@
 package org.example;
 
 import org.example.lab3.Parceptron;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import org.example.lab3.util.AlphabetMapping;
+import org.example.lab3.util.DataInitialization;
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(
-                new InputStreamReader(System.in));
+        AlphabetMapping mapping = new AlphabetMapping();
+        DataInitialization dataInitialization = new DataInitialization();
+
         final int maxEpochs = 2000;
         int n = 20;
+        int[] trainT = dataInitialization.getTraninT();
+        double[][] trainX = dataInitialization.getTraninX();
+
 
         Parceptron classifier = new Parceptron(n);
 
-        double[][] trainX = {
-                { 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // A
-                { 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Б
-                { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, // B
-                { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, // г
-                { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 }, // Ґ
-                { 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, // Д
-                { 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Е
-                { 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Є
-                { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0 }, // Ж
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, // З
-                { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // И
-                { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // І
-                { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 }, // Ї
-                { 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, // Й
-                { 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 }, // К
-                { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 }, // Л
-                { 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // М
-                { 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Н
-                { 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // О
-                { 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // П
-                { 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Р
-                { 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // С
-                { 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Т
-                { 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0 }, // У
-                { 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Ф
-                { 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 }, // Х
-                { 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Ц
-                { 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Ч
-                { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Ш
-                { 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Щ
-                { 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 }, // Ь
-                { 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Ю
-                { 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, // Я
-        };
-
-        int[] trainT = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                0,
-                0 };
-        System.out.println("Enter your letter");
-
-        String userLetter = reader.readLine().toUpperCase();
-
-        AlphabetMapping mapping = new AlphabetMapping();
-
-        int index = mapping.getIndex(userLetter);
-
-        trainT[index] = 1;
 
         classifier.train(trainX, trainT, maxEpochs);
 
         int indexOfPredictedLetter = 0;
+
         for (int i = 0; i < trainX.length; i++) {
             int predict = classifier.predict(trainX[i]);
             if (predict == 1) {
@@ -79,27 +35,7 @@ public class Main {
     }
 }
 
-// { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, //
-// 0- top straight horizontal line(‾)
-// 1- straight horizontal line(-)
-// 2- horizontal parallel 2 lines(=)
-// 3- straight vertical line(|)
-// 4- vertical parallel 2 lines(||)
-// 5- Triple Vertical Bar (⦀)
-// 6- right tilt(/)
-// 7- left tilt(\)
-// 8- right half of circle (◗)
-// 9- left half of circle (🤇)
-// 10- Combining Left Angle Below(̚)
-// 11- Combining right Angle Below(⸀)
-// 12- right Angle(_|)
-// 13- two dots(••)
-// 14- two vertical half of circles (like in 3)
-// 15- half of the circle at the top(◡)
-// 16- Combining a right angle from above(ꜚ)
-// 17- checkbox left(>)
-// 18- checkbox right(<)
-// 19- checkbox down(\/)
+
 
 // кон'юнкція, 2І, мінімум,
 // детектор 8
